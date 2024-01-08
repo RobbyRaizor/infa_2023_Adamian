9b940aba09606f2667d55d504e19d8146bbc5bfe
Station <RobbyRaizor2@gmail.com>
2024-01-08 21:14:09 +0300

    Added ":" after the request for surname.

--- start.py
+++ start.py
@@ -1,7 +1,7 @@
 print('Hello World!')
 
 name = str(input('Input your name: '))
-surname = str(input('Input your surname'))
+surname = str(input('Input your surname: '))
 age = int(input('Input your age: '))
 
 print(f'Hello, {surname} {name}! You are {age} y. o.')
